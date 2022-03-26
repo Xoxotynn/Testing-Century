e76fadcddee45115488dac177d9a9cc517390783
Edward <tamerlan701@gmail.com>
2022-03-26 07:22:47 +0700

    Initial Commit

---
+++ TestingCentury/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingCentury
+//
+//  Created by Эдуард Логинов on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
