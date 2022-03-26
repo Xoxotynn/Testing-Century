8852e84282feb78263e3414230cf03b75d043d5a
Edward <tamerlan701@gmail.com>
2022-03-26 08:27:51 +0700

    Solved task in whatCentury function in ViewController class

--- TestingCentury/SceneDelegate.swift
+++ TestingCentury/SceneDelegate.swift
@@ -4,10 +4,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
         
         window?.rootViewController = ViewController()
--- TestingCentury/ViewController.swift
+++ TestingCentury/ViewController.swift
@@ -1,9 +1,43 @@
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private let centuryLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        view.addSubview(centuryLabel)
+        
+        centuryLabel.text = whatCentury("2000")
+        centuryLabel.font = .systemFont(ofSize: 32)
+        centuryLabel.textAlignment = .center
+        centuryLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func whatCentury(_ year: String) -> String? {
+        guard let intYear = Int(year) else { return nil }
+        
+        let century = (intYear - 1) / 100 + 1
+        let endings = [
+            1: "st",
+            2: "nd",
+            3: "rd"
+        ]
+        
+        if ((century / 10) % 10 == 1 || (century >= 10 && century <= 20)) {
+            return "\(century)th"
+        }
+        
+        if let result = endings[century % 10] {
+            return "\(century)\(result)"
+        }
+        
+        return "\(century)th"
     }
 }
 
